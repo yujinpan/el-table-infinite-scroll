55cf0b0a1405e88dedf38ffb8eea31434fc2f079
yujinpan <1192878390@qq.com>
2019-11-06 18:10:59 +0800

    update examples

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,17 @@
 <template>
   <el-row id="app">
-    <el-col :span="12">
+    <el-col :span="8">
+      <ul>
+        <li>
+          1. 请设置 el-table 的高度，可以设置为
+          auto/100%(自适应高度)！不然会一直加载！
+        </li>
+        <li>
+          2. 右侧的列表为自适应高度，试试缩放浏览器的高度。
+        </li>
+      </ul>
+    </el-col>
+    <el-col :span="8">
       <el-table border v-el-table-infinite-scroll="load" :data="tableData">
         <el-table-column prop="date" label="日期" width="180">
         </el-table-column>
@@ -8,12 +19,8 @@
         </el-table-column>
         <el-table-column prop="address" label="地址"> </el-table-column>
       </el-table>
-      <el-button style="margin: 15px;" type="warning"
-        >请设置 el-table 的高度，可以设置为
-        auto/100%(自适应高度)！不然会一直加载！</el-button
-      >
     </el-col>
-    <el-col :span="12">
+    <el-col :span="8">
       <el-table
         border
         height="100%"
@@ -36,7 +43,7 @@ import elTableInfiniteScroll from '../src';
 const exampleData = new Array(10).fill({
   date: '2016-05-02',
   name: '王小虎',
-  address: '上海市普陀区金沙江路 1518 弄'
+  address: '王小虎'
 });
 
 export default {
